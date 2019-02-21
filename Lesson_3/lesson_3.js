b423e46a0187e2680bcf02ef47f2d413bf49f55e
John198303 <potekhine@gmail.com>
2019-02-21 09:35:55 +0300

    1

---
+++ Lesson_3/lesson_3.js
@@ -0,0 +1,21 @@
+// первое задание
+
+
+
+
+
+
+
+
+// Четвертое задание
+
+var x = 'x';
+var concat = 'x' + 'x';
+
+for (let i = 0; i < 20; i++) {
+
+    if (x === 'x') console.log(x);
+    x += concat;
+    console.log(x);
+
+}
